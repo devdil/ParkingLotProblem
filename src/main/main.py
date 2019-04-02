d1c9befd75c521ca0c9f7987f8fec095ae6eabc7
Diljit PR <diljitpr@gmail.com>
2019-04-02 17:37:02 +0530

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -2,15 +2,13 @@ from src.main.controllers.commandline_controller import CommandLineController
 from os import path
 import sys
 
-def start(filename):
+def start(filename, interactive=False):
     commandcontroller = CommandLineController()
-    if filename and path.exists(filename):
+    if filename and path.exists(filename) and not interactive:
         commandcontroller.start_non_interactive(filename)
     else:
         commandcontroller.start_interactive()
 
 if  __name__ == '__main__':
     if len(sys.argv) > 1:
-        start(sys.argv[1])
-    else:
-        start(None)
+        start(sys.argv[1], interactive=False)
